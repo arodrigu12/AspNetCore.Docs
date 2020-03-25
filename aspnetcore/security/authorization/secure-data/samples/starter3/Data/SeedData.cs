e7ef6199a8399af8d2cb1b16236aab815afdfbaf
Alfredo Rodriguez <arodrigu12@hotmail.com>
2020-03-25 18:31:14 -0400

    Added OwnerID and ContactStatus to Contact model; added Role services to Identity; set default authentication policy; excluded Index and Privacy pages from authentication; configured test accts fro admin and manager; created test accts and updated the contacts; added ContactOperations.cs.

--- aspnetcore/security/authorization/secure-data/samples/starter3/Data/SeedData.cs
+++ aspnetcore/security/authorization/secure-data/samples/starter3/Data/SeedData.cs
@@ -1,4 +1,6 @@
-﻿using ContactManager.Models;
+﻿using ContactManager.Authorization;
+using ContactManager.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -15,10 +17,76 @@ namespace ContactManager.Data
         {
             using (var context = new ApplicationDbContext(
                 serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
-            {              
-                SeedDB(context, "0");
+            {
+                // For sample purposes seed both with the same password.
+                // Password is set with the following:
+                // dotnet user-secrets set SeedUserPW <pw>
+                // The admin user can do anything
+
+                var adminID = await EnsureUser(serviceProvider, testUserPw, "admin@contoso.com");
+                await EnsureRole(serviceProvider, adminID, Constants.ContactAdministratorsRole);
+
+                // allowed user can create and edit contacts that they create
+                var managerID = await EnsureUser(serviceProvider, testUserPw, "manager@contoso.com");
+                await EnsureRole(serviceProvider, managerID, Constants.ContactManagersRole);
+
+                SeedDB(context, adminID);
             }
-        }        
+        }
+
+        private static async Task<string> EnsureUser(IServiceProvider serviceProvider,
+                                                    string testUserPw, string UserName)
+        {
+            var userManager = serviceProvider.GetService<UserManager<IdentityUser>>();
+
+            var user = await userManager.FindByNameAsync(UserName);
+            if (user == null)
+            {
+                user = new IdentityUser
+                {
+                    UserName = UserName,
+                    EmailConfirmed = true
+                };
+                await userManager.CreateAsync(user, testUserPw);
+            }
+
+            if (user == null)
+            {
+                throw new Exception("The password is probably not strong enough!");
+            }
+
+            return user.Id;
+        }
+
+        private static async Task<IdentityResult> EnsureRole(IServiceProvider serviceProvider,
+                                                                      string uid, string role)
+        {
+            IdentityResult IR = null;
+            var roleManager = serviceProvider.GetService<RoleManager<IdentityRole>>();
+
+            if (roleManager == null)
+            {
+                throw new Exception("roleManager null");
+            }
+
+            if (!await roleManager.RoleExistsAsync(role))
+            {
+                IR = await roleManager.CreateAsync(new IdentityRole(role));
+            }
+
+            var userManager = serviceProvider.GetService<UserManager<IdentityUser>>();
+
+            var user = await userManager.FindByIdAsync(uid);
+
+            if (user == null)
+            {
+                throw new Exception("The testUserPw password was probably not strong enough!");
+            }
+
+            IR = await userManager.AddToRoleAsync(user, role);
+
+            return IR;
+        }
 
         public static void SeedDB(ApplicationDbContext context, string adminID)
         {
@@ -28,6 +96,7 @@ namespace ContactManager.Data
             }
 
             context.Contact.AddRange(
+
                 new Contact
                 {
                     Name = "Debra Garcia",
@@ -35,8 +104,11 @@ namespace ContactManager.Data
                     City = "Redmond",
                     State = "WA",
                     Zip = "10999",
-                    Email = "debra@example.com"
+                    Email = "debra@example.com",
+                    Status = ContactStatus.Approved,
+                    OwnerID = adminID
                 },
+
                 new Contact
                 {
                     Name = "Thorsten Weinrich",
@@ -44,7 +116,9 @@ namespace ContactManager.Data
                     City = "Redmond",
                     State = "WA",
                     Zip = "10999",
-                    Email = "thorsten@example.com"
+                    Email = "thorsten@example.com",
+                    Status = ContactStatus.Submitted,
+                    OwnerID = adminID
                 },
              new Contact
              {
@@ -53,7 +127,9 @@ namespace ContactManager.Data
                  City = "Redmond",
                  State = "WA",
                  Zip = "10999",
-                 Email = "yuhong@example.com"
+                 Email = "yuhong@example.com",
+                 Status = ContactStatus.Rejected,
+                 OwnerID = adminID
              },
              new Contact
              {
@@ -62,7 +138,9 @@ namespace ContactManager.Data
                  City = "Redmond",
                  State = "WA",
                  Zip = "10999",
-                 Email = "jon@example.com"
+                 Email = "jon@example.com",
+                 Status = ContactStatus.Approved,
+                 OwnerID = adminID
              },
              new Contact
              {
@@ -71,7 +149,9 @@ namespace ContactManager.Data
                  City = "Redmond",
                  State = "WA",
                  Zip = "10999",
-                 Email = "diliana@example.com"
+                 Email = "diliana@example.com",
+                 Status = ContactStatus.Approved, //Added by AR because it was missing
+                 OwnerID = adminID
              }
              );
             context.SaveChanges();
--- aspnetcore/security/authorization/secure-data/samples/starter3/Startup.cs
+++ aspnetcore/security/authorization/secure-data/samples/starter3/Startup.cs
@@ -12,6 +12,8 @@ using ContactManager.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace ContactManager
 {
@@ -32,9 +34,20 @@ namespace ContactManager
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddRazorPages();
+
+            services.AddControllers(config =>
+            {
+                // using Microsoft.AspNetCore.Mvc.Authorization;
+                // using Microsoft.AspNetCore.Authorization;
+                var policy = new AuthorizationPolicyBuilder()
+                                 .RequireAuthenticatedUser()
+                                 .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
