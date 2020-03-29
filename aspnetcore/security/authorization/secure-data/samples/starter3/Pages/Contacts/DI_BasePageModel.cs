e6e71f4c5f05b150f64b6b52dadfd722ff0b678f
Alfredo Rodriguez <arodrigu12@hotmail.com>
2020-03-29 17:19:46 -0400

    Added ContactOperations class; created base class DI_BasePageModel for the Contacts razor pages; Updated the create page model constructor to use DI_BasePageModel; updated the CreateModel.OnPostAsync method.

---
+++ aspnetcore/security/authorization/secure-data/samples/starter3/Pages/Contacts/DI_BasePageModel.cs
@@ -0,0 +1,29 @@
+﻿using ContactManager.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContactManager.Pages.Contacts
+{
+    public class DI_BasePageModel : PageModel
+    {
+        // Adds the ApplicationDbContext.
+        protected ApplicationDbContext Context { get; }
+
+        // Adds the IAuthorizationService service to access the authorization handlers.
+        protected IAuthorizationService AuthorizationService { get; }
+
+        // Adds the Identity UserManager service.
+        protected UserManager<IdentityUser> UserManager { get; }
+
+        public DI_BasePageModel(
+            ApplicationDbContext context,
+            IAuthorizationService authorizationService,
+            UserManager<IdentityUser> userManager) : base()
+        {
+            Context = context;
+            UserManager = userManager;
+            AuthorizationService = authorizationService;
+        }
+    }
+}
